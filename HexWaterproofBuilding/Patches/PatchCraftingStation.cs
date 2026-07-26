ccf0b84e446281bc586d9904edbb10fd5661bd6c
devmail8181@gmail.com <devmail8181@gmail.com>
2026-07-26 12:18:20 -0600

    Add feature to remove roof requirement for workbench

--- HexWaterproofBuilding/Patches/PatchCraftingStation.cs
+++ HexWaterproofBuilding/Patches/PatchCraftingStation.cs
@@ -7,14 +7,14 @@ internal static class PatchCraftingStation
     [HarmonyPrefix]
     internal static void Prefix(CraftingStation __instance)
     {
-        if(Plugin.Instance == null)
+        if(Plugin.Instance == null || !Plugin.Instance.IsModEnabled || Plugin.Instance.IsWorkBenchRequireRoof)
         {
             return;
         }
 
         var prefabName = Utils.GetPrefabName(__instance.gameObject);
 
-        if (prefabName.Equals("piece_workbench"))
+        if (prefabName.Equals("piece_workbench") || prefabName.Equals("waterproof_piece_workbench", System.StringComparison.OrdinalIgnoreCase))
         {
             __instance.m_craftRequireRoof = false;
         }
